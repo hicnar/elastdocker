48bb3aa979aa24af5ebed6d142aa2a49c0bcb94e
Chris Rancyh <khycnar@o2.pl>
2023-05-14 00:38:09 +0100

    Experimenting with the project.

---
+++ reset-permissions.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+chown -R khycnar *
+
+chgrp -R users *
+
+# Set execute permissions for directories
+find . -type d -exec chmod 755 {} +
+
+# Set read permissions for non-executable files
+find . -type f ! -name "*.sh" -exec chmod 644 {} +
+
+# Set execute permissions for shell scripts
+find . -type f -name "*.sh" -exec chmod 755 {} +
